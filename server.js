ace2846f06fadef972ab02b926afadf86517123e
Medha Bobbili <medhabobbili@gmail.com>
2017-08-11 19:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+app.get('/article-one', function(req,res){
+    res.send('Article one requested and will be served here');
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
