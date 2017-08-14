f488e91a87b5a29a281aec470babe159a72e79a1
Medha Bobbili <medhabobbili@gmail.com>
2017-08-14 17:35:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,12 @@ element.innerHTML="New Value";
 
 
 //move the image
-var image = document.getElementById('madi');
-img.onclick = function(){
-    img.style.marginLeft ="100px";
+var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+var interval=setInterval(moveRight,50);
 }
