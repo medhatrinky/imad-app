17e47bf623700b1ff4e7c96747b8c344a487d863
Medha Bobbili <medhabobbili@gmail.com>
2017-08-14 19:00:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,26 @@ function moveRight(){
 img.onclick=function(){
 var interval=setInterval(moveRight,50);
 }
+
+
+//counter code 
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    //create a request object
+    var request=new XMLHTTPRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHTTPRequest.DONE){
+            //TAKE SOME ACTION
+            if (request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    //make request
+    request.open('GET','http://medhabobbili.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
