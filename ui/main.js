7ed2588e8bcccf8c7b197020adb5286ecc4ac5d9
Medha Bobbili <medhabobbili@gmail.com>
2017-08-15 14:17:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,3 @@
-console.log('Loaded!');
-
-
-//change the text of the main text div
-var element =document.getElementById('main-text');
-
-element.innerHTML="New Value";
-
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-var interval=setInterval(moveRight,50);
-}
 
 
 //counter code 
@@ -27,7 +8,7 @@ button.onclick=function(){
     var request=new XMLHTTPRequest();
     //capture the response and store it in a variable
     request.onreadystatechange=function(){
-        if(request.readyState===XMLHTTPRequest.DONE){
+        if(request.readyState===XMLHttpRequest.DONE){
             //TAKE SOME ACTION
             if (request.status===200){
                 var counter=request.responseText;
