2d6dd21b9b82373771804b9bcd123dd8db27e57f
Medha Bobbili <medhabobbili@gmail.com>
2017-08-15 15:22:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     //get the name from request
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
    res.send 
