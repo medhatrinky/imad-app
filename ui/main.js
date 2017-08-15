41de08e4d9b23ab2e2ab9628526d1cfae6e4d5d7
Medha Bobbili <medhabobbili@gmail.com>
2017-08-15 17:02:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
-//counter code 
-var button=document.getElementById('counter');
 
-button.onclick=function(){
+
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    //make request to the server and send the name 
+    //capture a list of names and render it as a list
     //create a request object
     var request=new XMLHttpRequest();
     //capture the response and store it in a variable
@@ -12,28 +14,26 @@ button.onclick=function(){
                 var names=request.responseText;
                 names=JSON.parse(names);
               
-    var list='';
-    for(i=0;i<names.length;i++)
-    {
-        list+='<li>' + names[i] +'</li>'
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+                var list='';
+                for(i=0;i<names.length;i++)
+                    {
+                   list+='<li>' + names[i] +'</li>'
+                       }
+                      var ul=document.getElementById('namelist');
+                         ul.innerHTML=list;
  
             }
         }
     };
-    //make request
-    request.open('GET','http://medhabobbili.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
-};
+   
+
 
 
 // submit Name
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
-    //make request to the server and send the name 
-    //capture a list of names and render it as a list
+ //make request
+    request.open('GET','http://medhabobbili.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+
 };  
